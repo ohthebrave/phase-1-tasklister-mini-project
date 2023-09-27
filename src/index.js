9595857be63151c0979d6057a7c94f4dd5abcc68
George <georgejoj28@gmail.com>
2023-09-27 15:36:01 +0300

    Added interactivity

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form');
+  // console.log(form)
+  form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    buildTodo(e.target.new_task_description.value)
+    let input = document.getElementById('new_task_description')
+    input.value = " ";
+  })
 });
+
+const buildTodo = (myTodos) => {
+  // console.log(myTodos)
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', (e)=> {
+    e.target.parentNode.remove()
+  })
+  btn.textContent = 'X'
+  li.textContent = `${myTodos} `;
+  li.appendChild(btn);
+  // console.log(li);
+  document.querySelector('#tasks').appendChild(li);
+}
+
+
